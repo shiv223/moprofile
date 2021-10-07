e731bb2b42852374af43ece9cddad038bcb105f4
shiv <kasyap.aditya223@gmail.com>
2021-10-07 23:50:51 +0530

    Add files via upload

---
+++ src/home/Home.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import './css/bootstrap.min.css';
+import './css/gh-fork-ribbon.min.css';
+import './css/homepage.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="home-container">
+                <section id="backgroundblur"></section>
+<section>
+    <div className="container">
+    	<div className="row">
+    	    
+    		<div className="col-md-4">
+    		    <div className="card profile-card-1">
+    		        <img src="https://images.pexels.com/photos/946351/pexels-photo-946351.jpeg?w=500&h=650&auto=compress&cs=tinysrgb" alt="profile-sample1" className="background"/>
+    		        <img src="./img/gugu.jpg" alt="profile-image" className="profile"/>
+                    <div className="card-content">
+                    <h2>Sibabrata Acharya<small>Product Team Lead</small></h2>
+                    <div className="icon-block"><a href="https://moprofiles.com/sacharya/"><i className="fa fa-id-card"></i></a><a href="https://moprofiles.com/homepage/sacharya/"> <i className="fa fa-linkedin"></i></a><a href="https://moprofiles.com/homepage/sacharya/"> <i className="fa fa-github"></i></a></div>
+                    </div>
+                </div>
+                <p className="mt-3 w-100 float-left text-center"><strong> </strong></p>
+    		</div>		
+
+    		<div className="col-md-4">
+    		    <div className="card profile-card-3">
+    		        <div className="background-block">
+    		            <img className="https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="profile-sample1" className="background"/>
+    		        </div>
+    		        <div className="profile-thumb-block">
+    		            <img src="img/biswajit.jpg" alt="profile-image" className="profile"/>
+    		        </div>
+    		        <div className="card-content">
+                    <h2>Biswajit Mahapatra<small>Python Data Engineer</small></h2>
+                    <div className="icon-block"><a href="https://moprofiles.com/biswajit/"><i className="fa fa-id-card"></i></a><a href="#"> <i className="fa fa-linkedin"></i></a><a href="#"> <i className="fa fa-github"></i></a></div>
+                    </div>
+                </div>
+                <p className="mt-3 w-100 float-left text-center"><strong> </strong></p>
+    		</div>
+
+    		<div className="col-md-4">
+    		    <div className="card profile-card-1">
+    		        <img src="https://images.pexels.com/photos/946351/pexels-photo-946351.jpeg?w=500&h=650&auto=compress&cs=tinysrgb" alt="profile-sample1" className="background"/>
+    		        <img src="img/sasmita.jpg" alt="profile-image" className="profile"/>
+                    <div className="card-content">
+                    <h2>Sasmita Satpathy<small>Senior Consultant</small></h2>
+                    <div className="icon-block"><a href="https://moprofiles.com/sasmita/"><i className="fa fa-id-card"></i></a><a href="#"> <i className="fa fa-linkedin"></i></a><a href="#"> <i className="fa fa-github"></i></a></div>
+                    </div>
+                </div>
+                <p className="mt-3 w-100 float-left text-center"><strong> </strong></p>
+    		</div>
+    		
+    		<div className="col-md-4">
+    		    <div className="card profile-card-1">
+    		        <img src="https://images.pexels.com/photos/946351/pexels-photo-946351.jpeg?w=500&h=650&auto=compress&cs=tinysrgb" alt="profile-sample1" className="background"/>
+    		        <img src="img/bishal.jpg" alt="profile-image" className="profile"/>
+                    <div className="card-content">
+                    <h2>Bishal Hembram<small>Engineer</small></h2>
+                    <div className="icon-block"><a href="https://moprofiles.com/bishal/"><i className="fa fa-id-card"></i></a><a href="#"> <i className="fa fa-linkedin"></i></a><a href="#"> <i className="fa fa-github"></i></a></div>
+                    </div>
+                </div>
+                <p className="mt-3 w-100 float-left text-center"><strong> </strong></p>
+    		</div>
+    		
+    		<div className="col-md-4">
+    		    <div className="card profile-card-1">
+    		        <img src="https://images.pexels.com/photos/946351/pexels-photo-946351.jpeg?w=500&h=650&auto=compress&cs=tinysrgb" alt="profile-sample1" className="background"/>
+    		        <img src="img/ayudh.jpg" alt="profile-image" className="profile"/>
+                    <div className="card-content">
+                    <h2>Ayudh Bhanja Deo<small>Senior Analyst</small></h2>
+                    <div className="icon-block"><a href="https://moprofiles.com/ayudh/"><i className="fa fa-id-card"></i></a><a href="#"> <i className="fa fa-linkedin"></i></a><a href="#"> <i className="fa fa-github"></i></a></div>
+                    </div>
+                </div>
+                <p className="mt-3 w-100 float-left text-center"><strong> </strong></p>
+    		</div>
+
+			<div className="col-md-4">
+    		    <div className="card profile-card-1">
+    		        <img src="https://images.pexels.com/photos/946351/pexels-photo-946351.jpeg?w=500&h=650&auto=compress&cs=tinysrgb" alt="profile-sample1" className="background"/>
+    		        <img src="https://randomuser.me/api/portraits/women/20.jpg" alt="profile-image" className="profile"/>
+                    <div className="card-content">
+                    <h2>Savannah Fields<small>Engineer</small></h2>
+                    <div className="icon-block"><a href="#"><i className="fa fa-id-card"></i></a><a href="#"> <i className="fa fa-linkedin"></i></a><a href="#"> <i className="fa fa-github"></i></a></div>
+                    </div>
+                </div>
+                <p className="mt-3 w-100 float-left text-center"><strong> </strong></p>
+    		</div>	
+            		<div className="col-md-4">
+    		    <div className="card profile-card-1">
+    		        <img src="https://images.pexels.com/photos/946351/pexels-photo-946351.jpeg?w=500&h=650&auto=compress&cs=tinysrgb" alt="profile-sample1" className="background"/>
+    		        <img src="https://randomuser.me/api/portraits/women/20.jpg" alt="profile-image" className="profile"/>
+                    <div className="card-content">
+                    <h2>Savannah Fields<small>Engineer</small></h2>
+                    <div className="icon-block"><a href="#"><i className="fa fa-id-card"></i></a><a href="#"> <i className="fa fa-linkedin"></i></a><a href="#"> <i className="fa fa-github"></i></a></div>
+                    </div>
+                </div>
+                <p className="mt-3 w-100 float-left text-center"><strong> </strong></p>
+    		</div>
+            		<div className="col-md-4">
+    		    <div className="card profile-card-1">
+    		        <img src="https://images.pexels.com/photos/946351/pexels-photo-946351.jpeg?w=500&h=650&auto=compress&cs=tinysrgb" alt="profile-sample1" className="background"/>
+    		        <img src="https://randomuser.me/api/portraits/women/20.jpg" alt="profile-image" className="profile"/>
+                    <div className="card-content">
+                    <h2>Savannah Fields<small>Engineer</small></h2>
+                    <div className="icon-block"><a href="#"><i className="fa fa-id-card"></i></a><a href="#"> <i className="fa fa-linkedin"></i></a><a href="#"> <i className="fa fa-github"></i></a></div>
+                    </div>
+                </div>
+                <p className="mt-3 w-100 float-left text-center"><strong> </strong></p>
+    		</div>
+            		<div className="col-md-4">
+    		    <div className="card profile-card-1">
+    		        <img src="https://images.pexels.com/photos/946351/pexels-photo-946351.jpeg?w=500&h=650&auto=compress&cs=tinysrgb" alt="profile-sample1" className="background"/>
+    		        <img src="https://randomuser.me/api/portraits/women/20.jpg" alt="profile-image" className="profile"/>
+                    <div className="card-content">
+                    <h2>Savannah Fields<small>Engineer</small></h2>
+                    <div className="icon-block"><a href="#"><i className="fa fa-id-card"></i></a><a href="#"> <i className="fa fa-linkedin"></i></a><a href="#"> <i className="fa fa-github"></i></a></div>
+                    </div>
+                </div>
+                <p className="mt-3 w-100 float-left text-center"><strong> </strong></p>
+    		</div>
+    	</div>
+   </div> </section>
+</div>
+        )
+    }
+}
+
+export default Home;
